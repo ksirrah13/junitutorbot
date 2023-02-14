08e1f36134ab8a1521de900321a977a3d7c1e640
Kyle Harris <kyle@learnwithjuni.com>
2023-02-14 14:53:51 -0800

    display all text from pods from worlfram

--- wolfram.js
+++ wolfram.js
@@ -1,6 +1,7 @@
 import { recordNewResponse } from "./data_storage.js";
 import WolframApi from '@tanzanite/wolfram-alpha';
-import { processWorlframPods, sendTextResponse } from './worlfram_utils.js';
+import { processWorlframPods } from './worlfram_utils.js';
+import { createEmbedWrapper, createEmbedImages } from './discord_utils.js';
 
 
 // TODO add a precheck that verifies wolfram can answer the question
@@ -19,43 +20,49 @@ export const doWolfram = async (prompt, thread, parentPromptId) => {
   try {
     const precheckResult = await wolframPrecheck(prompt);
     console.dir('wolfram precheck', {precheckResult});
+    const worlframClassification = {domain: precheckResult.domain, confidence: precheckResult.resultsignificancescore};
     if (precheckResult.accepted === 'false') {
       console.log('wolfram failed precheck');
       const invalidWolframQuery = "Worlfram can't handle this type of query";
       const responseId = await recordNewResponse({ prompt, response: invalidWolframQuery, source: 'wolfram', parentPromptId });
-      await sendTextResponse(invalidWolframQuery, thread, responseId);
+      await sendTextResponse(invalidWolframQuery, thread, responseId, worlframClassification);
       return invalidWolframQuery;
     }
-    const worlframClassification = {domain: precheckResult.domain, confidence: precheckResult.resultsignificancescore};
     const waApi = WolframApi(process.env.WOLFRAM_APP_ID);
     const queryResult = await waApi
-      .getFull({ input: prompt, output: 'json', podstate: 'Step-by-step solution' });
-    // const queryResult = await waApi
-    //   .getSimple(prompt);
+      .getFull({ input: prompt, output: 'json', podstate: 'Step-by-step solution' }); // TODO find better podstate search agorithm
 
-    let result = '';
     if (!queryResult.success) {
-      if (queryResult.didyoumeans && queryResult.didyoumeans.val) {
-        result = `Did you mean ${queryResult.didyoumeans.val} ?`;
-      } else {
-        result = 'unsuccessful response from api';
-      }
+      const result = queryResult.didyoumeans && queryResult.didyoumeans.val
+        ? `Did you mean ${queryResult.didyoumeans.val} ?`
+        : 'Unsuccessful response from api'
       console.log('wolfram queryResult', queryResult);
       const responseId = await recordNewResponse({ prompt, response: result, source: 'wolfram', parentPromptId });
       await sendTextResponse(result, thread, responseId, worlframClassification);
       return result;
     }
-    const podResults = await processWorlframPods(queryResult, thread, worlframClassification);
-    if (!podResults) {
-      console.error('no pods in response', queryResult);
-      const responseId = await recordNewResponse({ prompt, response: 'no results in response', source: 'wolfram', parentPromptId });
-      await sendTextResponse('no results in response', thread, responseId, worlframClassification);
-      return 'no results in response';
-    }
-    return podResults;
+    const podResults = processWorlframPods(queryResult, thread, worlframClassification);
+    if (podResults) {
+      const responseId = await recordNewResponse({ prompt, response: podResults, source: 'wolfram', parentPromptId });
+      await sendTextResponse(podResults, thread, responseId, worlframClassification);
+      return podResults;
+    } 
+    console.error('No pods in response', queryResult);
+    const responseId = await recordNewResponse({ prompt, response: 'No results in response', source: 'wolfram', parentPromptId });
+    await sendTextResponse('no results in response', thread, responseId, worlframClassification);
+    return 'No results in response';
   } catch (error) {
     console.error(error);
     throw error;
   }
+}
+
+const sendTextResponse = async (result, thread, responseId, classification) => {
+  const annotatedResult = classification ? `Problem classification: ${classification.domain} (confidence: ${classification.confidence})\n\n${result}` : result;
+  await thread.send(createEmbedWrapper('Wolfram', annotatedResult, responseId));
+}
+
+const sendImageResponse = async (images, thread) => {
+  await thread.send({ embeds: [createEmbedImages('Wolfram', images)] });
 }
 
--- worlfram_utils.js
+++ worlfram_utils.js
@@ -1,41 +1,26 @@
-import { createEmbedWrapper, createEmbedImages } from './discord_utils.js';
-import { recordNewResponse } from './data_storage.js'
-
-export const processWorlframPods = async (queryResult, thread, worlframClassification) => {
+export const processWorlframPods = (queryResult) => {
     if (queryResult && queryResult.pods) {
         const pods = queryResult.pods;
-        pods.forEach(pod => processSinglePod(pod, thread, worlframClassification));
-        return 'found something?';
+        const result = pods.map(pod => processSinglePod(pod)).join('\n\n');
+        return result;
       }
 }
 
-export const sendTextResponse = async (result, thread, responseId, classification) => {
-    const annotatedResult = classification ? `Problem classification: ${classification.domain} (confidence: ${classification.confidence})\n\n${result}` : result;
-    await thread.send(createEmbedWrapper('Wolfram', annotatedResult, responseId));
-  }
-  
-export const sendImageResponse = async (images, thread) => {
-    await thread.send({ embeds: [createEmbedImages('Wolfram', images)] });
-  }
+const processSinglePod = (pod) => {
+    const subpods = pod.subpods;
+    const podTitle = pod.title;
+    const result = subpods.map(subpod => processPlainText(subpod)).join('\n\n');
+    if (result) {
+        return `## ${podTitle} ##\n${result}`;
+    }
+}
+    
 
-const processSinglePod = async (pod, thread, worlframClassification) => {
-    const resultSubPods = pod.subpods;
-        const stepsPods = resultSubPods.filter(pod => pod.title === "Possible intermediate steps");
-        if (stepsPods.length === 1) {
-          const stepsText = stepsPods[0].plaintext;
-        //   const responseId = await recordNewResponse({ prompt, response: stepsText, source: 'wolfram', parentPromptId });
-          await sendTextResponse(stepsText, thread, '', worlframClassification);
-          return stepsText;
-        }
-        console.log('no intermediate pods', resultSubPods);
-        // no step by step so get the first plain text pod response
-        const firstPod = resultSubPods[0];
-        const resultText = firstPod.plaintext;
-        // const responseId = await recordNewResponse({ prompt, response: resultText, source: 'wolfram', parentPromptId });
-        if (resultText) {
-            await sendTextResponse(resultText, thread, '', worlframClassification);
-        }
-        return resultText;
-
+const processPlainText = (subpod) => {
+    const text = subpod.plaintext;
+    const title = subpod.title;
+    if (text) {
+        return `${title ? `# ${title}\n` : ''}${text}`;
+    }
 }
   
